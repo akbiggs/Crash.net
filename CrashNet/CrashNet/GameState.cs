7d38ae96781c4cd76dc292659062408ea081c7e2
Alexander Biggs <spirit.of.mana@gmail.com>
2012-07-16 13:57:17 -0400

    Started main game class, added some skeletons.
    Created main game class and a GameState enum, with skeletons for levels,
    interfaces and menus.

---
+++ CrashNet/CrashNet/GameState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CrashNet
+{
+    enum GameState
+    {
+        Cutscene,
+        Level,
+        Boss,
+        GameMenu,
+        MainMenu
+    }
+}
